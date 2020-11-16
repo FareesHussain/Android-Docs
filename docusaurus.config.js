805166ef4b0fe9ef6aa8fe9e122d339caa44980e
Farees Hussain <fareezzhussain@gmail.com>
2020-11-17 03:30:04 +0530

    trackingid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()} Android Docs, Built by Farees Hussain.`,
     },
     googleAnalytics: {
-      trackingID: 'UA-6NJQ14VNP0-1',
+      trackingID: 'UA-6NJQ14VNP0',
       // Optional fields.
       anonymizeIP: true, // Should IPs be anonymized?
     },
