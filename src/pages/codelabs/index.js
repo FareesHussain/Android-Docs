61071c6e92a6e15e1a12087b58b4a6711caff096
Farees Hussain <fareezzhussain@gmail.com>
2020-11-16 19:17:43 +0530

    styling

--- src/pages/codelabs/index.js
+++ src/pages/codelabs/index.js
@@ -87,22 +87,11 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Codelabs`}
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
+          <h1 className="hero__title">CODELABS</h1>
         </div>
       </header>
       <main>
